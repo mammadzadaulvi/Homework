ac0d0a51c73d2f07b2624f8cd9b6cbcf3a0fb7d7
mammadzadaulvi <ulvifm@code.edu.az>
2022-05-17 18:38:52 +0400

    Reference

---
+++ task_referance/ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("Nurlan Faiq Emil Tural Yusif"));
+        }
+
+        static string Reverse(string soz)
+        {
+            char[] herf = soz.ToCharArray();
+            string ReversedString = "";
+
+            for (int i = herf.Length - 1; i > -1; i--)
+            {
+                ReversedString += herf[i];
+            }
+            return ReversedString;
+        }
+    }
+}
