0e4038e421f676e7a3004d2832ae199e8e844cae
mammadzadaulvi <ulvifm@code.edu.az>
2022-05-24 01:09:47 +0400

    Constructor

---
+++ task_Constructor/ConsoleApp8/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    class Person
+    {
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+
+            IsAdult = false;
+            if (age > 18)
+            {
+                IsAdult = true;
+            }
+        }
+
+        public string Name;
+        public string Surname;
+        public int Age;
+        public bool IsAdult;
+
+    }
+}
---
+++ task_Constructor/ConsoleApp8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Akif", "Memmedov", 20);
+            Console.WriteLine(person1.Name + " " + person1.Surname + " " + person1.Age + " " + person1.IsAdult);
+
+
+            Person person2 = new Person("Abbas", "Eliyev", 17);
+            Console.WriteLine(person2.Name + " " + person2.Surname + " " + person2.Age + " " + person2.IsAdult);
+        }
+    }
+}
